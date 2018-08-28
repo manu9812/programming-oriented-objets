d68cd6ffc1c41b6574d7c40a6ba5094e62783fe2
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-08-28 14:55:48 -0500

    abstraccion

---
+++ abstraccion/Persona.java
@@ -0,0 +1,26 @@
+/*
+ en esta clase voy a crear los atributos
+de nombre de nombre y edad de las personas
+que son los empleados
+ */
+package abstraccion;
+
+/**
+ *
+ * @author  Manuela garcía Monsalve 
+ * 28 de agosto del 2018
+ */
+public abstract  class Persona {
+    String nombre;
+    int edad;
+    public Persona(String nom, int ed){
+    this.nombre=nom;
+    this.edad=ed;
+        
+    }
+    public void VerDatos(){
+        
+    }
+
+    
+}
---
+++ abstraccion/abstraccion.java
@@ -0,0 +1,21 @@
+/*
+
+ */
+package abstraccion;
+
+/**
+ *
+ * @author Manuela garcía Monsalve 
+ * 28 de agosto del 2018
+ */
+public class abstraccion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // esta es la clase donde vamos a crear nuestros objetos 
+       Empleado empleado1= new Empleado("jose", 223,"8743627834","suba",80000);
+    }
+    //pedir por consola  
+}
